5690e7d463c38235b67901a0bdf7ba70d2acc432
Jason Sim <jason.young.sim@gmail.com>
2024-02-29 16:26:31 -0600

    u

--- PythonTemplate/OrphanPagesSpark.py
+++ PythonTemplate/OrphanPagesSpark.py
@@ -21,7 +21,7 @@ def id_counter(line):
     return results
 
 lines = lines.map(id_counter)
-lines = lines.reduceByKey(lambda x, y : x)
+# lines = lines.reduceByKey(lambda x, y : x)
 
 LOGGER.info(f'{str(lines.take(N))}>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
 output = open(sys.argv[2], "w")
