27ab45d3e2ef6e232f9e612008c5c33a22cf16f0
Jason Sim <jason.young.sim@gmail.com>
2024-02-29 10:06:54 -0600

    u

--- PythonTemplate/TitleCountSpark.py
+++ PythonTemplate/TitleCountSpark.py
@@ -24,18 +24,21 @@ conf.set("spark.driver.bindAddress", "127.0.0.1")
 sc = SparkContext(conf=conf)
 
 lines = sc.textFile(sys.argv[3], 1)
-lines = lines.flatMap(lambda x: x.split(' '))
-lines = lines.filter(lambda x: x not in stop_words)
-lines = lines.map(lambda x: (x, 1))
-lines = lines.reduceByKey(lambda x, y: x + y)
-lines = lines.sortBy(lambda x: x[1], ascending=False)
+
+#TODO
+tokenized_words = []
+for line in lines:
+    words = line.lower()
+    for delimiter in delimiters:
+        words = ' '.join(words.split(delimiter))
+    words = [word for word in words.split() if word not in stop_words]
+    tokenized_words.extend(words)
 
 outputFile = open(sys.argv[4],"w")
 
 #TODO
 #write results to output file. Foramt for each line: (line +"\n")
-for line in lines.collect():
-    outputFile.write(line + "\n")
+for word in tokenized_words:
+    outputFile.write(word + "\n")
 
-outputFile.close()
 sc.stop()
