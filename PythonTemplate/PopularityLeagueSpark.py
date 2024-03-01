354facdc4794b6f8bbae8ea8f8ffdcb4c3f6f71d
Jason Sim <jason.young.sim@gmail.com>
2024-02-29 21:29:09 -0600

    u

--- PythonTemplate/PopularityLeagueSpark.py
+++ PythonTemplate/PopularityLeagueSpark.py
@@ -33,7 +33,7 @@ lines = lines.filter(lambda x : x[0] in league)
 lines = lines.map(lambda x : (x[1], set([x[0]])))
 lines = lines.reduceByKey(lambda x, y : x.union(y))
 lines = lines.sortBy(lambda x : x[0], ascending=True)
-lines = lines.map(lambda x : (x[1], x[0]))
+lines = lines.map(lambda x : (x[1], len(x[1])))
 
 # lines = lines.map(lambda x : (x[1], x[0]))
 
