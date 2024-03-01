625431dc07d562db6e4b392032def714056a5ec9
Jason Sim <jason.young.sim@gmail.com>
2024-02-29 20:53:14 -0600

    u

--- PythonTemplate/PopularityLeagueSpark.py
+++ PythonTemplate/PopularityLeagueSpark.py
@@ -32,6 +32,7 @@ leagueIds = sc.textFile(sys.argv[2], 1)
 league = leagueIds.collect()
 lines = lines.filter(lambda x : x[0] in league)
 lines = lines.sortBy(lambda x : x[1], ascending=False)
+lines = lines.map(lambda x : (x[1], x[0]))
 LOGGER.info(f'{str(lines.collect())}>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
 
 output = open(sys.argv[3], "w")
