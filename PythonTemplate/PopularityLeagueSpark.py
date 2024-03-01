6e387d49ea89d5e01b4e3879270805f37115e125
Jason Sim <jason.young.sim@gmail.com>
2024-02-29 21:38:36 -0600

    u

--- PythonTemplate/PopularityLeagueSpark.py
+++ PythonTemplate/PopularityLeagueSpark.py
@@ -31,8 +31,8 @@ leagueIds = sc.textFile(sys.argv[2], 1)
 league = leagueIds.collect()
 lines = lines.filter(lambda x : x[0] in league)
 lines = lines.sortBy(lambda x : x[1], ascending=True)
-# lines = lines.map(lambda x : (x[1], set([x[0]])))
-# lines = lines.reduceByKey(lambda x, y : x.union(y))
+lines = lines.map(lambda x : (x[1], set([x[0]])))
+lines = lines.reduceByKey(lambda x, y : x.union(y))
 
 
 
