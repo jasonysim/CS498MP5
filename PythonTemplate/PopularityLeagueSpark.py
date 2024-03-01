2eb11c560fe87798d8a1cb35c182d66a5413e2b1
Jason Sim <jason.young.sim@gmail.com>
2024-02-29 21:26:01 -0600

    u

--- PythonTemplate/PopularityLeagueSpark.py
+++ PythonTemplate/PopularityLeagueSpark.py
@@ -31,7 +31,7 @@ leagueIds = sc.textFile(sys.argv[2], 1)
 league = leagueIds.collect()
 lines = lines.filter(lambda x : x[0] in league)
 lines = lines.map(lambda x : (x[1], [x[0]]))
-lines = lines.reduceByKey(lambda x, y : [x] + [y])
+lines = lines.reduceByKey(lambda x, y : [x] + y)
 # lines = lines.sortBy(lambda x : x[0], ascending=True)
 
 def level_counter(line):
